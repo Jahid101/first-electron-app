bac0b39000f3fdf584d51a7e472314bd4a432d10
Md Jahid Hasan Anik <jahid.prolific@gmail.com>
2024-07-11 12:03:26 +0600

    btn UI added

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('node:path')
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     })
 
     win.loadFile('index.html')
@@ -11,6 +15,7 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
     console.log('App is up and running')
+    ipcMain.handle('ping', () => 'pong')
     createWindow()
 
     // Open a window if none are open (macOS)
