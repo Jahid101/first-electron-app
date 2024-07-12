e2f138234bf5a80d31db41b39053f770f01fdd0e
Md Jahid Hasan Anik <jahid.prolific@gmail.com>
2024-07-12 11:00:47 +0600

    functionality added

--- renderer/js/testScript.js
+++ renderer/js/testScript.js
@@ -1,8 +1,32 @@
 var info = document.getElementById('info')
+var btn1 = document.getElementById('btn1')
+var btn2 = document.getElementById('btn2')
+
 info.innerText = '0';
 
-document.getElementById('btn1').addEventListener('click', () => {
-    alert('Btn pressed')
+if (localStorage.getItem('total')) {
+    info.innerText = localStorage.getItem('total') || 0
+}
+
+btn1.addEventListener('click', () => {
+    const total = localStorage.getItem('total') || 0
+
+    if (total == 0) {
+        alert("Enough is enough!!! You want me to die now !!!")
+        return
+    }
+
+    if (total && total > 0) {
+        localStorage.setItem('total', +total - 1)
+        info.innerText = localStorage.getItem('total')
+    } else {
+        localStorage.setItem('total', 0)
+        info.innerText = 0
+    }
+
+})
+
+btn2.addEventListener('click', () => {
     const total = localStorage.getItem('total') || 0
     localStorage.setItem('total', +total + 1)
     info.innerText = localStorage.getItem('total')
